ec49df9e4e7298b9a2a8345c5483e5eb11e3f29f
Ian Weaver <iweaver@ucsc.edu>
2014-03-28 19:26:13 -0500

    ian's first commit

--- app/src/main/java/com/superslug/myapplication3/app/MainActivity.java
+++ app/src/main/java/com/superslug/myapplication3/app/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item); //ian
     }
 
 }
